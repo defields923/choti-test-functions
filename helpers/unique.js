722b359f7fbf9d6c17be438834c8c286cf62501b
Devin Fields <devin.fields@mrcooper.com>
2020-09-11 20:10:56 -0500

    Add Unique and test file

---
+++ helpers/unique.js
@@ -0,0 +1,31 @@
+// DONT GOOGLE THIS!!!!!!!!!!!
+
+function _uniqueWithFilter(array) {
+  // YOUR CODE HERE
+  // SOLVE BY REUSING YOUR _filter AND _indexOf METHODS
+
+}
+
+function _uniqueWithObject() {
+  const memo = {};
+
+  // YOUR CODE HERE
+  // NO LOOPING AT ALL!!!
+  // SOLVE BY MAKING USE OF THE MEMO OBJECT ABOVE
+  // ASK YOURSELF HOW AN OBJECT CAN HELP YOU DETERMINE IF A VALUE IS ALREADY INSIDE OF YOUR NEW ARRAY.
+
+}
+
+function _uniqueWithSet() {
+  // Make a Set and solve the same problem with the Set
+
+  // YOUR CODE HERE
+  // NO LOOPING AT ALL (STILL)!!!
+
+}
+
+module.exports = {
+  _uniqueWithFilter,
+  _uniqueWithObject,
+  _uniqueWithSet
+};
