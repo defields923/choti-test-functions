fef576f63c1279ac65ad9f6d12f17cf9aa401fea
Devin Fields <devin.fields@mrcooper.com>
2020-09-23 15:38:22 -0500

    Remove uniqueWithObject and assert cases

--- helpers/unique.js
+++ helpers/unique.js
@@ -6,16 +6,6 @@ function _uniqueWithFilter(array) {
 
 }
 
-function _uniqueWithObject() {
-  const memo = {};
-
-  // YOUR CODE HERE
-  // NO LOOPING AT ALL!!!
-  // SOLVE BY MAKING USE OF THE MEMO OBJECT ABOVE
-  // ASK YOURSELF HOW AN OBJECT CAN HELP YOU DETERMINE IF A VALUE IS ALREADY INSIDE OF YOUR NEW ARRAY.
-
-}
-
 function _uniqueWithSet() {
   // Make a Set and solve the same problem with the Set
 
--- test/unique.test.js
+++ test/unique.test.js
@@ -8,36 +8,23 @@ describe('Unique', () => {
     const startingList = [5, 4, 5, 7, 7, 7, 9, 'a', {}, {}, 'b', 5, 2, true, true];
 
     const result1 = helpers._uniqueWithFilter(startingList);
-    const result2 = helpers._uniqueWithObject(startingList);
-    const result3 = helpers._uniqueWithSet(startingList);
+    const result2 = helpers._uniqueWithSet(startingList);
 
     assert.equal(result1.length, 10);
-    assert.equal(result2.length, 10);
-    assert.equal(result3.length, 10);
-
     assert.equal(result1[0], 5);
-    assert.equal(result2[0], 5);
-    assert.equal(result3[0], 5);
-
     assert.equal(result1[1], 4);
-    assert.equal(result2[1], 4);
-    assert.equal(result3[1], 4);
-
     assert.equal(result1[2], 7);
-    assert.equal(result2[2], 7);
-    assert.equal(result3[2], 7);
-
     assert.equal(result1[3], 9);
-    assert.equal(result2[3], 9);
-    assert.equal(result3[3], 9);
-
     assert.equal(result1[4], 'a');
-    assert.equal(result2[4], 'a');
-    assert.equal(result3[4], 'a');
-
     assert.equal(result1[result1.length - 1], true);
+
+    assert.equal(result2.length, 10);
+    assert.equal(result2[0], 5);
+    assert.equal(result2[1], 4);
+    assert.equal(result2[2], 7);
+    assert.equal(result2[3], 9);
+    assert.equal(result2[4], 'a');
     assert.equal(result2[result2.length - 1], true);
-    assert.equal(result3[result3.length - 1], true);
   });
 });
 
